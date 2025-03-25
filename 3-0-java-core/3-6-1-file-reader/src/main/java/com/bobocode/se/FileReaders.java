dd0fd0ce7ddbe26c9c3b536559799c379ca4fb4f
JaverJaver <mykhailo.kornukh@gmail.com>
2025-03-25 20:44:12 +0200

    FileReaders refactor

--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -5,13 +5,16 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
  */
 public class FileReaders {
+    private static final String DELIMITER = "\n";
 
     /**
      * Returns a {@link String} that contains whole text from the file specified by name.
@@ -22,16 +25,17 @@ public class FileReaders {
     public static String readWholeFile(String fileName) {
         try {
             URI resource = getResource(fileName);
-            return Files.lines(Paths.get(resource))
-                    .collect(Collectors.joining("\n"));
+            try (Stream<String> lines = Files.lines(Paths.get(resource))) {
+                return lines.collect(Collectors.joining(DELIMITER));
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Cannot read file: " + fileName, e);
         }
     }
 
     private static URI getResource(String fileName) {
-        URL resource = FileReaders.class.getClassLoader().getResource(fileName);
         try {
+            URL resource = FileReaders.class.getClassLoader().getResource(fileName);
             return resource.toURI();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
